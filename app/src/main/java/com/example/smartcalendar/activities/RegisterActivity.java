7396fedf822ece2928a5b29a9dca79541d54b52a
MinhVKhanh <minhvu933205@gmail.com>
2023-11-14 18:09:57 +0700

    change

--- app/src/main/java/com/example/smartcalendar/activities/RegisterActivity.java
+++ app/src/main/java/com/example/smartcalendar/activities/RegisterActivity.java
@@ -88,7 +88,7 @@ public class RegisterActivity extends LoginActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.setStatusBarColor(Color.TRANSPARENT);
+
             window.setStatusBarColor(getResources().getColor(R.color.colorAccent));
         }
     }
